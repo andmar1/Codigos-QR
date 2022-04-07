48d116afed809e2b68f357d0f30ea65c30772640
Antonio Andrade <antoniomaresmares001gmail.com>
2022-04-06 22:38:54 -0500

    last version

--- CodigosQR JS/app.js
+++ CodigosQR JS/app.js
@@ -1,1 +1,14 @@
-const 
+const contenedorQR = document.getElementById('contenedorQR');
+
+// new QRCode( contenedorQR, 'https://blogandmar.netlify.app' );  objeto sin ser dinamico, con url definida
+
+const formulario = document.getElementById('formulario')
+
+const QR = new QRCode( contenedorQR );
+
+formulario.addEventListener('submit', ( e ) =>{
+    e.preventDefault();
+    // acceder al valor del input
+    QR.makeCode( formulario.link.value );
+});
+
